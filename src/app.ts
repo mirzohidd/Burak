f065556e2ed4140349923493105c12ec05f11b7d
mirzohidd <mirzohidmurodov77@gmail.com>
2024-12-24 22:30:12 +1000

    feat:sessions

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,14 @@ import router from "./router";
 import routerAdmin from "./routerAdmin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/config";
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore({
+  uri: String(process.env.MONGO_URL),
+  collection: "sessions",
+});
 /** 1-ENTRANCE **/
 const app = express();
 console.log("__dirname:", __dirname);
@@ -12,7 +20,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
 /** 2-SESSIONS **/
-
+app.use(
+  require("express-session")({
+    secret: String(process.env.SESSION_SECRET),
+    cookie: {
+      maxAge: 1000 * 3600 * 3, // 3 h
+    },
+    store: store,
+    // Boilerplate options, see:
+    // * https://www.npmjs.com/package/express-session#resave
+    // * https://www.npmjs.com/package/express-session#saveuninitialized
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 /** 3-VIEWS **/
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
