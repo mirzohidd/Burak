bdf962448ff54cbe35f769431f2eaaa7c72495e7
mirzohidd <mirzohidmurodov77@gmail.com>
2025-01-07 14:38:41 +1000

    feat:image validation and preview

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,5 +1,31 @@
 console.log("Signup frontend javascript file");
 
+$(function () {
+  const filetTarget = $(".file-box .upload-hidden");
+  let filename;
+
+  filetTarget.on("change", function () {
+    if (window.FileReader) {
+      const uploadFile = $(this)[0].files[0];
+      console.log("uploadFile:", uploadFile);
+      const fileType = uploadFile["type"];
+      const validateImageType = ["image/jpg", "image/jpeg", "image/png"];
+      if (!validateImageType.includes(fileType)) {
+        alert("Please insert only jpeg ,jpg and png!");
+      } else {
+        if (uploadFile) {
+          console.log(URL.createObjectURL(uploadFile));
+          $(".upload-img-frame")
+            .attr("src", URL.createObjectURL(uploadFile))
+            .addClass("success");
+        }
+        filename = $(this)[0].files[0].name;
+      }
+
+      $(this).siblings(".upload-name").val(filename);
+    }
+  });
+});
 
 function validateSignupForm() {
   const memberNick = $(".member-nick").val();
@@ -18,6 +44,15 @@ function validateSignupForm() {
   }
   if (memberPassword !== confirmPassword) {
     alert("Password differs,please check !");
+    return false;
+  }
+
+  const memberImage = $(".member-image").get(0).files[0]
+    ? $(".member-image").get(0).files[0].name
+    : null;
+
+  if (!memberImage) {
+    alert("Please insert restaurant image !");
     return false;
   }
 }
