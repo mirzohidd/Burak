19a221d03a86f0a14a54d6afec335c19ad7c6d46
mirzohidd <mirzohidmurodov77@gmail.com>
2025-01-07 14:20:44 +1000

    feat:signup process front validation

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -59,7 +59,7 @@ class MemberService {
     const exist = await this.memberModel
       .findOne({ memberType: MemberType.RESTAURANT })
       .exec();
-    if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+    if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);  
     console.log("before", input.memberPassword);
     const salt = await bcrypt.genSalt();
     input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
