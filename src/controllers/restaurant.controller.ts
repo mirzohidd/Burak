5802ff1ec677758a7f0fbed233c90e3b926404d9
mirzohidd <mirzohidmurodov77@gmail.com>
2025-01-09 00:44:00 +1000

    fix:users page render real data

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -107,7 +107,7 @@ restaurantController.getUsers = async (req: Request, res: Response) => {
   try {
     console.log("getUsers");
     const result = await memberService.getUsers();
-
+    console.log("result",result)
     res.render("users", { user: result });
   } catch (err) {
     console.log("Error , getUsers", err);
